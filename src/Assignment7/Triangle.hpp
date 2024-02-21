8ef63c17eaaf78212c03dd6bbc162064d98e91fd
Scotchingice <1439921564@qq.com>
2024-02-21 13:06:11 +0800

    something

--- src/Assignment7/Triangle.hpp
+++ src/Assignment7/Triangle.hpp
@@ -253,7 +253,14 @@ inline Intersection Triangle::getIntersection(Ray ray)
     t_tmp = dotProduct(e2, qvec) * det_inv;
 
     // TODO find ray triangle intersection
-
+    if (t_tmp < 0)
+        return inter;
+    inter.distance = t_tmp;
+    inter.m = m;
+    inter.obj = this;
+    inter.happened = true;
+    inter.normal = normal;
+    inter.coords = ray(t_tmp);
     return inter;
 }
 
